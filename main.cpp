85e1f940c489ae5afb39069fb6f9c7fa38ca0a57
轻字节编程(Light Byte Code) <111341725+LightByteCode@users.noreply.github.com>
2023-01-28 09:27:35 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include<graphics.h>
 #include<iostream>
-#include<ctime>
+#include<vector>
+#include<string>
 
 using namespace std;
 //全局变量
 MOUSEMSG msg;//全局鼠标信息变量
 IMAGE mine, grid, grid_motion, grid_down, flag, flag_motion , mine_other;//所有图像变量
 //单个格子大小(单位：像素)，雷数常量，格子布局长宽(单位：格子)，窗口宽度，窗口高度
-const int grid_size = 40, layout_size=10, width = layout_size * grid_size, height = layout_size * grid_size;
+int grid_size = 40, layout_size = 10, width = 400, height = 400;
 
 int all_num = 0, grid_x = 0, grid_y = 0, mines_num = 10;
 
@@ -29,94 +30,11 @@ public:
 		this->i = i; this->j = j;
 		//绘制
 		change(grid);
-		//判断自己的位置(grids二维数组记录周围8个格子的行和列)
-		if (i == 0 && j == 0)
-		{
-			//左上角
-			int grids[3][2] = { {0,1},{1,0},{1,1}};
-			for (int a = 0; a < 3; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else if (i == layout_size - 1 && j == layout_size - 1)
-		{
-			//右下角
-			int grids[3][2] = { {layout_size - 1,layout_size - 2},{layout_size - 2,layout_size - 1},{layout_size - 2,layout_size - 2}};
-			for (int a = 0; a < 3; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else if (i == 0 && j == layout_size - 1)
-		{
-			//右上角
-			int grids[3][2] = { {0,layout_size - 2},{1,layout_size - 1},{1,layout_size - 2}};
-			for (int a = 0; a < 3; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else if (i == layout_size - 1 && j == 0){
-			//左下角
-			int grids[3][2] = { {layout_size - 1,layout_size - 2},{layout_size - 2,layout_size - 2},{layout_size-2,layout_size-1} };
-			for (int a = 0; a < 3; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-			
-		else if (i == 0 && j > 0 && j < layout_size - 1) 
-		{
-			//上-边
-			int grids[8][2] = { {0,j - 1},{0,j + 1},{1,j - 1},{1,j},{1,j + 1},{2,j - 1},{2,j},{2,j + 1} };
-			for (int a = 0; a < 8; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else if (i == layout_size - 1 && j > 0 && j < layout_size - 1) 
-		{
-			//下-边
-			int grids[8][2] = { {layout_size - 1,j - 1},{layout_size - 1,j + 1} ,{layout_size - 2,j - 1} ,{layout_size - 2,j} ,{layout_size - 2,j + 1} ,{layout_size - 3,j - 1} ,{layout_size - 3,j} ,{layout_size - 3,j + 1} };
-			for (int a = 0; a < 8; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-			
-		else if (j == 0 && i > 0 && i < layout_size - 1) 
-		{
-			//左-边
-			int grids[8][2] = { {i - 1,0} ,{i - 1,1} ,{i - 1,2} ,{i,1} ,{i,2} ,{i + 1,0} ,{i + 1,1} ,{i + 1,2} };
-			for (int a = 0; a < 8; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else if (j == layout_size - 1 && i > 0 && i < layout_size - 1) 
-		{
-			//右-边
-			int grids[8][2] = { {i - 1,layout_size - 1} ,{i - 1,layout_size - 2} ,{i - 1,layout_size - 3} ,{i,layout_size - 2} ,{i,layout_size - 3} ,{i + 1,layout_size - 1} ,{i + 1,layout_size - 2} ,{i + 1,layout_size - 3} };
-			for (int a = 0; a < 8; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
-		}
-
-		else
-		{
-			//以此格子为中心
-			int grids[8][2] = { {i - 1,j - 1} ,{i - 1,j} ,{i - 1,j + 1} ,{i,j - 1} ,{i,j + 1} ,{i + 1,j - 1} ,{i + 1,j} ,{i + 1,j + 1} };
-			for (int a = 0; a < 8; a++) {
-				this->grids[a][0] = grids[a][0];
-				this->grids[a][1] = grids[a][1];
-			}
+		//以此格子为中心
+		int grids[8][2] = { {i - 1,j - 1} ,{i - 1,j} ,{i - 1,j + 1} ,{i,j - 1} ,{i,j + 1} ,{i + 1,j - 1} ,{i + 1,j} ,{i + 1,j + 1} };
+		for (int a = 0; a < 8; a++) {
+			this->grids[a][0] = grids[a][0];
+			this->grids[a][1] = grids[a][1];
 		}
 	}
 
@@ -143,7 +61,7 @@ public:
 		outtextxy(x1 + 6, y1 - 3, _T(info));
 	}
 
-	void find_0(Grid(&layout)[layout_size][layout_size]) {
+	void find_0(vector<vector<Grid>> &layout) {
 		for (int a = 0; a < 8; a++) {//遍历grids（周围格子）
 			int i = grids[a][0], j = grids[a][1];
 			if (layout[i][j].num == 0 && layout[i][j].is_click == false && layout[i][j].is_mine == false) {
@@ -158,37 +76,121 @@ public:
 	}
 };
 
-int main() {
+class Button {
+public:
+	int x, y, width, height;
+	const char* text="NULL";
+
+	void show(const char* text, int x, int y, int width, int height, COLORREF color1 = WHITE)
+	{
+		this->x = x; this->y = y;
+		this->width = width; this->height = height;
+		this->text = text;
+
+		setlinecolor(color1);//设置框边颜色
+		setbkmode(TRANSPARENT);//设置字体背景透明
+		setfillcolor(color1);//设置填充颜色
+		fillroundrect(x, y, x + width, y + height, 10, 10);//画一个按钮框
+
+		settextcolor(BLACK);
+		settextstyle(40, 0, "黑体");
+		int tx = x + (width - textwidth(text)) / 2;
+		int ty = y + (height - textheight(text)) / 2;
+		outtextxy(tx, ty, text);
+	}
+
+	void change(COLORREF color2=RGB(237, 237, 237)) {
+		setlinecolor(color2);//设置框边颜色
+		setfillcolor(color2);//设置填充颜色
+		fillroundrect(x, y, x + width, y + height, 10, 10);//画一个按钮框
+
+		settextcolor(BLACK);
+		settextstyle(40, 0, "黑体");
+		int tx = x + (width - textwidth(text)) / 2;
+		int ty = y + (height - textheight(text)) / 2;
+		outtextxy(tx, ty, text);
+	}
+};
+
+void settings() {
+	int temp=0;
+	cout << "设置格子大小(单位：像素)" << endl;
+	cin >> grid_size;
+	cout << "设置雷盘边长(单位：格子)" << endl;
+	cin >> layout_size;
+	width = height = layout_size * grid_size;
+	cout << width << ' ' << height << endl;
+	cout << "设置雷数" << endl;
+	cin >> temp;
+	if (temp >= layout_size * layout_size)
+		cout << "设置的雷太多了" << endl;
+	else {
+		mines_num = temp;
+		cout << "OK 开始！" << endl;
+		return;
+	}
+}
+
+void start() {
 	//程序初始化
 	initgraph(width, height);
-	setbkcolor(WHITE);
+	setbkcolor(BLACK);
 	cleardevice();
+	setbkcolor(WHITE);
+	//绘制设置按钮
+
+	Button btn1;
+	btn1.show("设置", 150, 100, 100, 50);
+	Button btn2;
+	btn2.show("开始", 150, 200, 100, 50);
+	//死循环
+	while (true) {
+		ExMessage msg = getmessage(EX_MOUSE);
+
+		if (msg.message == WM_LBUTTONDOWN) {
+			if (msg.x >= btn1.x && msg.x <= btn1.x + btn1.width && msg.y >= btn1.y && msg.y <= btn1.y + btn1.height) {
+				btn1.change();
+				settings();
+				cleardevice();
+				initgraph(width, height);
+				return;
+			}
+
+			else if (msg.x >= btn2.x && msg.x <= btn2.x + btn2.width && msg.y >= btn2.y && msg.y <= btn2.y + btn2.height) {
+				btn2.change();
+				cleardevice();
+				return;
+			}
+		}
+	}
+}
+
+int main() {
+	//程序初始化
+	start();
 	//加载图像
 	loadimage(&mine, "images/mine.png", grid_size, grid_size);
 	loadimage(&grid, "images/grid.png", grid_size, grid_size);
-	loadimage(&grid_motion, "images/grid_motion.png", grid_size, grid_size);
 	loadimage(&grid_down, "images/grid_down.png", grid_size, grid_size);
 	loadimage(&flag, "images/flag.png", grid_size, grid_size);
-	loadimage(&flag_motion, "images/flag_motion.png", grid_size, grid_size);
-	loadimage(&mine_other, "images/mine_other.png", grid_size, grid_size);
 	//布雷
-	Grid layout[width / grid_size][height / grid_size];//布局，用二维数组，如果是1就有雷，0则没有
+	vector<vector<Grid>> layout(layout_size+2, vector<Grid>(layout_size+2));//布局，用二维数组，如果是1就有雷，0则没有
 
 	srand(time(nullptr));
 	//初始化
-	for (int i = 0; i < layout_size; i++) {
-		for (int j = 0; j < layout_size; j++) {
+	for (int i = 1; i < layout_size+1; i++) {
+		for (int j = 1; j < layout_size+1; j++) {
 			layout[i][j].init(false, grid_x, grid_y, i, j);
-			grid_x += 40;
+			grid_x += grid_size;
 		}
-		grid_y += 40;
+		grid_y += grid_size;
 		grid_x = 0;
 	}
 	//随机布雷实现
 	int x = 0, y = 0, count = 0;
 	for (count = 0; count < mines_num;){
-		x = 1 + rand() % layout_size-1;
-		y = 1 + rand() % layout_size-1;
+		x = 1 + rand() % layout_size+1;
+		y = 1 + rand() % layout_size+1;
 		if (layout[x][y].is_mine == false){
 			layout[x][y].is_mine = true;
 			count++;//每次布置好一个雷之后,才会计数
@@ -205,24 +207,24 @@ int main() {
 			}
 		}
 	}
-	/*调试输出布局*/
+	/*调试输出布局
 	for (int i = 0; i < layout_size; i++) {
 		for (int j = 0; j < layout_size; j++) {
 			cout << layout[i][j].is_mine << ' ';
 		}
 		cout << endl;
-	}
+	}*/
 	//轮询监测鼠标事件
 	while (true) {
 		//监测鼠标操作
 		ExMessage msg = getmessage(EX_MOUSE);
 
 		if (msg.message == WM_LBUTTONDOWN) {
-			for (int i = 0; i < layout_size; i++) {
-				for (int j = 0; j < layout_size; j++) {
+			for (int i = 1; i < layout_size+1; i++) {
+				for (int j = 1; j < layout_size+1; j++) {
 					//通过点击坐标判断是哪个格子
 					if (msg.x >= layout[i][j].x1 && msg.x <= layout[i][j].x2 && msg.y >= layout[i][j].y1 && msg.y <= layout[i][j].y2) {
-						if (layout[i][j].is_click == false) {//判断是否被点击过
+						if (layout[i][j].is_click == false) { //判断是否被点击过
 							layout[i][j].is_flag = false;
 							//如果这个格子是雷
 							if (layout[i][j].is_mine) {
@@ -270,39 +272,9 @@ int main() {
 			}
 		}
 
-		if (msg.message == WM_MOUSEMOVE) {
-			for (int i = 0; i < layout_size; i++) {
-				for (int j = 0; j < layout_size; j++) {
-					//通过坐标判断是哪个格子
-					if (msg.x >= layout[i][j].x1 && msg.x <= layout[i][j].x2 && msg.y >= layout[i][j].y1 && msg.y <= layout[i][j].y2) {
-						if (layout[i][j].is_click) {
-							layout[i][j].change(grid_down);
-							layout[i][j].show_num();
-						}
-						else if (layout[i][j].is_click == false && layout[i][j].is_flag == false)
-							layout[i][j].change(grid_motion);
-						else if (layout[i][j].is_click == false && layout[i][j].is_flag == true)
-							layout[i][j].change(flag_motion);
-					}
-
-					else {
-						if (layout[i][j].is_click) {
-							layout[i][j].change(grid_down);
-							layout[i][j].show_num();
-						}
-						else if (layout[i][j].is_click == false && layout[i][j].is_flag == false)
-							layout[i][j].change(grid);
-						else if (layout[i][j].is_click == false && layout[i][j].is_flag)
-							layout[i][j].change(flag);
-					}
-
-				}
-			}
-		}
-
 		else if (msg.message == WM_RBUTTONDOWN) {
-			for (int i = 0; i < layout_size; i++) {
-				for (int j = 0; j < layout_size; j++) {
+			for (int i = 1; i < layout_size+1; i++) {
+				for (int j = 1; j < layout_size+1; j++) {
 					//通过点击坐标判断是哪个格子
 					if (msg.x >= layout[i][j].x1 && msg.x <= layout[i][j].x2 && msg.y >= layout[i][j].y1 && msg.y <= layout[i][j].y2) {
 						//如果已经被标记就取消标记
